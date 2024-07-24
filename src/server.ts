47643cfd356d164089e46c7d72f202d3ee6d981f
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-07-24 14:24:27 -0300

    added error message on the movie creating

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ const app = express();
 const port = 3000;
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 app.get("/movies", async (_, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
@@ -13,10 +15,28 @@ app.get("/movies", async (_, res) => {
         include: {
             genres: true,
             languages: true,
-            
         },
     });
     res.json(movies);
+});
+
+app.post("/movies", async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+    try {
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            },
+        });
+        res.status(201).send();
+    } catch (error) {
+        res.status(500).send({message: "Error to create a new movie register"});
+    }
 });
 
 app.listen(port, () => {
